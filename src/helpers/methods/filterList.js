e37d2e5a5642a3dd57c1deace8bde46e753fc321
felipejuzo02 <felipejuzo@hotmail.com>
2022-05-25 17:32:48 -0300

    remove parse int in the filtersList

--- src/helpers/methods/filterList.js
+++ src/helpers/methods/filterList.js
@@ -1,5 +1,5 @@
 export default function (originalList, languageQuantity = 0) {
   return originalList.filter(item => {
-    return item.languages.length >= parseInt(languageQuantity)
+    return item.languages.length >= languageQuantity
   })
 }
